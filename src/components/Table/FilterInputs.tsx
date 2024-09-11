a6ca7a77cfbe4b1e48717bbfbfa29eaa4ce02d06
Tary131 <taras.pincak@seznam.cz>
2024-09-11 16:53:58 +0200

    feat(filter): add FilterInputs component for dynamic table filtering

---
+++ src/components/Table/FilterInputs.tsx
@@ -0,0 +1,61 @@
+import { FC, ChangeEvent } from 'react';
+
+interface FilterInputsProps {
+  filters: any;
+  handleFilterChange: (e: ChangeEvent<HTMLInputElement>, key: string) => void;
+}
+
+const FilterInputs: FC<FilterInputsProps> = ({
+  filters,
+  handleFilterChange,
+}) => (
+  <tr>
+    <th className="p-2">
+      <input
+        type="text"
+        placeholder="Filter by name"
+        value={filters.name}
+        onChange={(e) => handleFilterChange(e, 'name')}
+        className="w-full p-2 border rounded"
+      />
+    </th>
+    <th className="p-2">
+      <input
+        type="text"
+        placeholder="Filter by grade"
+        value={filters.grade}
+        onChange={(e) => handleFilterChange(e, 'grade')}
+        className="w-full p-2 border rounded"
+      />
+    </th>
+    <th className="p-2">
+      <input
+        type="text"
+        placeholder="Filter by subject"
+        value={filters.subject}
+        onChange={(e) => handleFilterChange(e, 'subject')}
+        className="w-full p-2 border rounded"
+      />
+    </th>
+    <th className="p-2">
+      <input
+        type="text"
+        placeholder="Filter by class"
+        value={filters.class}
+        onChange={(e) => handleFilterChange(e, 'class')}
+        className="w-full p-2 border rounded"
+      />
+    </th>
+    <th className="p-2">
+      <input
+        type="text"
+        placeholder="Filter by date"
+        value={filters.date}
+        onChange={(e) => handleFilterChange(e, 'date')}
+        className="w-full p-2 border rounded"
+      />
+    </th>
+  </tr>
+);
+
+export default FilterInputs;
