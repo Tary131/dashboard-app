e81a5875c4afea866ea4f8adba49e97da61a76ca
Tary131 <taras.pincak@seznam.cz>
2024-09-11 16:04:50 +0200

    style(Card): justifying h2 to the center

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,8 +11,10 @@ const Card: FC<CardProps> = ({ title, content, className = '' }) => {
     <div
       className={`shadow-lg rounded-lg p-4 bg-white border border-gray-200 transform transition-transform duration-300 hover:scale-105 ${className}`}
     >
-      <h2 className="text-xl font-semibold mb-2">{title}</h2>
-      <p>{content}</p>
+      <h2 className="flex justify-center text-xl font-semibold mb-2">
+        {title}
+      </h2>
+      <p className="flex justify-center">{content}</p>
     </div>
   );
 };
