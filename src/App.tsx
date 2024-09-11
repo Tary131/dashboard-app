d82a6187e7823c1ebf3db464249980b1a8f5244b
Tary131 <taras.pincak@seznam.cz>
2024-09-11 15:36:00 +0200

    feat(app): add page navigation with state management
    - Implemented `useState` to manage active page state (`activePage`)
    - Added new components for "Dashboard", "Subjects", and "Settings" pages
    - Created `pages` object to map `PageKey` to corresponding components
    - Integrated Sidebar to allow dynamic switching between pages
    - Render appropriate page content based on the current `activePage` value
    t

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
-// src/App.tsx
-import React from 'react';
+import { useState, ReactElement, FC } from 'react';
+import Sidebar from './components/Sidebar/Sidebar.tsx';
+import Students from './components/Students/Students.tsx';
 
-import SidebarComponent from './components/Sidebar/Sidebar.tsx';
+type PageKey = 'dashboard' | 'subjects' | 'students' | 'settings';
+const Dashboard: FC = () => <div>Dashboard Page</div>;
+const Subjects: FC = () => <div>Subjects Page</div>;
+const Settings: FC = () => <div>Settings Page</div>;
+const pages: Record<PageKey, ReactElement> = {
+  students: <Students />,
+  dashboard: <Dashboard />,
+  subjects: <Subjects />,
+  settings: <Settings />,
+};
 
-const App: React.FC = () => {
-  return <SidebarComponent />;
+const App: FC = () => {
+  const [activePage, setActivePage] = useState<PageKey>('students');
+  return (
+    <div className="flex flex-col h-screen">
+      <div className="flex flex-grow">
+        <Sidebar setActivePage={setActivePage} />
+        <main className="flex-1 p-5">
+          <h1 className="text-2xl font-semibold flex justify-center">
+            Main Content Area
+          </h1>
+          {pages[activePage]}
+        </main>
+      </div>
+    </div>
+  );
 };
 
 export default App;
