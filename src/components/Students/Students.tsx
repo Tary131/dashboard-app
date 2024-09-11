619bb67f1562e826e9a0365b6e55fc11a36cfe0a
Tary131 <taras.pincak@seznam.cz>
2024-09-11 20:25:33 +0200

    feat(students): integrate GradePieChart and StatisticsChart into card layout

--- src/components/Students/Students.tsx
+++ src/components/Students/Students.tsx
@@ -1,27 +1,29 @@
 import Card from '../Card.tsx';
 import StudentsTable from '../Table/StudentsTable.tsx';
+import StatisticsChart from '../Charts/StatisticsChart.tsx';
+import GradePieChart from '../Charts/GradePieChart.tsx';
 
 const Students = () => {
   return (
     <div className="grid gap-4 p-5 h-full grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6">
       <Card
         title="Students in class"
-        content="This card 1"
+        content={<GradePieChart />}
         className="row-span-2 lg:col-span-2 md:col-span-2 sm:col-span-2"
       />
       <Card
         title="Statistics"
-        content="This card 2"
+        content={<StatisticsChart />}
         className="lg:col-span-4 md:col-span-4 sm:col-span-2"
       />
       <Card
         title="Best students"
-        content="This card 3"
+        content="John Doe"
         className="lg:col-span-2 md:col-span-2 sm:col-span-2"
       />
       <Card
         title="Worst students"
-        content="This card 4"
+        content="Sam Johnson"
         className="lg:col-span-2 md:col-span-2 sm:col-span-2"
       />
       <Card
