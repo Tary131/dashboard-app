0890edd1939355a536a5eb9bb33d3efb6573e6c1
Tary131 <taras.pincak@seznam.cz>
2024-09-22 14:08:59 +0200

    feat: add function to count students by class

---
+++ src/components/Charts/getClassStudentCounts.tsx
@@ -0,0 +1,25 @@
+import { Student } from '../Table/FormattedStudentData.tsx';
+export const getClassStudentCounts = (students: Student[]) => {
+  const classStudentCount: { [key: string]: { [name: string]: number } } = {};
+
+  students.forEach((student) => {
+    const className = student.class || 'Unknown Class';
+    if (!classStudentCount[className]) {
+      classStudentCount[className] = {};
+    }
+
+    if (classStudentCount[className][student.name]) {
+      classStudentCount[className][student.name] += 1;
+    } else {
+      classStudentCount[className][student.name] = 1;
+    }
+  });
+
+  return Object.keys(classStudentCount).map((className) => ({
+    className,
+    students: Object.values(classStudentCount[className]).reduce(
+      (total, count) => total + count,
+      0
+    ),
+  }));
+};
