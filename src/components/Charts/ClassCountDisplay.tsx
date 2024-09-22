b08eb6a50ac7e2714ae4669948eb028083534664
Tary131 <taras.pincak@seznam.cz>
2024-09-22 14:07:13 +0200

    feat: add ClassCountDisplay component to display total number of classes

---
+++ src/components/Charts/ClassCountDisplay.tsx
@@ -0,0 +1,21 @@
+import { FC, useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '../../redux/hooks';
+import { fetchClasses } from '../../redux/thunks/classesThunks.ts';
+
+const ClassCountDisplay: FC = () => {
+  const dispatch = useAppDispatch();
+  const { totalCount, loading, error } = useAppSelector(
+    (state) => state.classes
+  );
+
+  useEffect(() => {
+    dispatch(fetchClasses());
+  }, [dispatch]);
+
+  if (loading) return <p>Loading Classes...</p>;
+  if (error) return <p>Error: {error}</p>;
+
+  return <p>{totalCount}</p>;
+};
+
+export default ClassCountDisplay;
