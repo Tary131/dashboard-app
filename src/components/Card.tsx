68c59d3680e4f15d5d98a1cec71fe30c7a773720
Tary131 <taras.pincak@seznam.cz>
2024-10-08 13:15:12 +0200

    style(Card): improve hover effect and shadow

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,7 +9,7 @@ interface CardProps {
 const Card: FC<CardProps> = ({ title, content, className = '' }) => {
   return (
     <div
-      className={`shadow-lg rounded-lg p-4 bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700 transform transition-transform duration-300 hover:scale-105 ${className} overflow-hidden`}
+      className={`shadow-lg rounded-lg p-4 bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700 transform transition-transform duration-300 hover:shadow-xl ${className} overflow-hidden`}
     >
       <h2 className="flex justify-center text-xl font-semibold mb-2 text-gray-900 dark:text-gray-100">
         {title}
